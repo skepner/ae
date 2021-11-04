ca76dcd297f7d1097f1b8073564ad33c96cb9201
Eugene Skepner <github@skepner.eu>
2021-11-04 15:59:07 +0100

    parsing ncbi files

--- py/ae/sequences/source/ncbi.py
+++ py/ae/sequences/source/ncbi.py
@@ -24,20 +24,27 @@ class reader:
         self.na_dat = self.read_influenza_na_dat(self.na_dat_filename)
         for en in self.reader_:
             context = Context(self, filename=self.fna_filename, line_no=en.line_no)
-            metadata = self.read_fna_name(en.name, context=context)
-            yield metadata, en.sequence # metadata may contain "excluded" key to manually exclude the sequence
+            if metadata := self.read_fna_name(en.name, context=context):
+                yield metadata, en.sequence # metadata may contain "excluded" key to manually exclude the sequence
 
     # ----------------------------------------------------------------------
 
     def read_fna_name(self, name: str, context: Context):
-        return {"name": name}
+        fields = name.split("|")
+        if len(fields) == 5:
+            if metadata := self.na_dat.get(fields[3]):
+                return metadata
+            # if not found, it most probably means wrong segment (not HA)
+        else:
+            print(f">> [ncbi] invalid fna name: \"{name}\"", file=sys.stderr)
+        return None
 
     # ----------------------------------------------------------------------
 
     def read_influenza_na_dat(self, filename: Path):
         raw_data = ae_backend.read_file(filename)
-        metadata = [entry for entry in (self.read_influenza_na_dat_entry(filename=filename, line_no=line_no, line=line) for line_no, line in enumerate(io.StringIO(raw_data), start=1)) if entry]
-        # print(f">>> {len(metadata)} rows read from {filename}")
+        metadata = {entry["sample_id_by_sample_provider"]: entry for entry in (self.read_influenza_na_dat_entry(filename=filename, line_no=line_no, line=line) for line_no, line in enumerate(io.StringIO(raw_data), start=1)) if entry}
+        # print(f">>> {len(metadata)} rows read from {filename}", file=sys.stderr)
         return metadata
 
         # print(f"{filename}: {len(data)}")
@@ -46,11 +53,11 @@ class reader:
         context = Context(self, filename=filename, line_no=line_no)
         fields = line.split("\t")
         if len(fields) != 11:
-            print(f">> invalid number of fields ({len(fields)}) @@ {filename}:{line_no}")
+            print(f">> invalid number of fields ({len(fields)}) @@ {filename}:{line_no}", file=sys.stderr)
             return None
         if len(fields) == 11 and fields[2] == "4" and (name := self.extract_name(fields[7])): # interested in segment 4 (HA) only
             # genbank_accession, host, segment_no, subtype, country, date, sequence_length, virus_name, age, gender, completeness
-            # print(f">>>> fields {fields}")
+            # print(f">>>> id \"{fields[0]}\"", file=sys.stderr)
             metadata = {
                 "sample_id_by_sample_provider": fields[0],
                 # "host": fields[1],
@@ -68,7 +75,7 @@ class reader:
             metadata["name"] = parse_name(name, metadata=metadata, context=context)
             if date := parse_date(fields[5], metadata=metadata, context=context):
                 metadata["date"] = date
-            # print(f">>>> metadata {metadata}")
+            # print(f">>>> metadata {metadata}", file=sys.stderr)
             return metadata
         else:
             return None
@@ -80,12 +87,11 @@ class reader:
         return country
 
     def extract_name(self, name):
-        if name[:19].upper() in ["INFLUENZA A VIRUS (", "INFLUENZA B VIRUS ("] and name[-1] == ")":
-            return name[19:-1]
-        elif name[:18].upper() in ["INFLUENZA A VIRUS(", "INFLUENZA B VIRUS("] and name[-1] == ")":
-            return name[18:-1]
-        else:
-            # print(f">>>> excluded name \"{name}\"")
-            return None
+        try:
+            if name[:17].upper() in ["INFLUENZA A VIRUS", "INFLUENZA B VIRUS"] and (paren := name.index("(")) in [17, 18] and name[-1] == ")":
+                return name[paren+1:-1]
+        except ValueError:
+            pass
+        return None
 
 # ----------------------------------------------------------------------
