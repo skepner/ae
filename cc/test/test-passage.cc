c1b642b3ee195eeb0f67c396ec50df25e764f470
Eugene Skepner <github@skepner.eu>
2021-11-11 15:16:52 +0100

    passage parser

---
+++ cc/test/test-passage.cc
@@ -0,0 +1,69 @@
+#include "ext/fmt.hh"
+#include "ext/range-v3.hh"
+#include "utils/messages.hh"
+#include "virus/passage-parse.hh"
+
+static size_t passage_parsing_test(bool verbose);
+
+// ======================================================================
+
+int main(int argc, const char* const* argv)
+{
+    const bool verbose{argc > 1 && std::string_view{argv[1]} == "-v"};
+    try {
+        return static_cast<int>(passage_parsing_test(verbose));
+    }
+    catch (std::exception& err) {
+        fmt::print("> {}\n", err.what());
+        return 1967;
+    }
+}
+
+// ----------------------------------------------------------------------
+
+struct D
+{
+    std::string_view raw_name;
+    std::string_view expected;
+};
+
+size_t passage_parsing_test(bool verbose)
+{
+    const std::array data{
+        D{"Original", "OR"}, //
+        D{"C1", "MDCK1"},    //
+        D{"MDCK1", "MDCK1"}, //
+    };
+
+    size_t errors = 0;
+    ae::virus::passage::parse_settings settings{ae::virus::passage::parse_settings::tracing::no};
+    for (const auto [no, entry] : ranges::views::enumerate(data)) {
+        try {
+            ae::Messages messages;
+            const auto result = ae::virus::passage::parse(entry.raw_name, settings, messages, ae::MessageLocation{"test", no});
+            // if (verbose)
+            //     fmt::print(">>>  \"{}\"\n", entry.raw_name);
+            if (!messages.empty()) {
+                fmt::print("> \"{}\"\n{}", entry.raw_name, messages.report());
+                ++errors;
+            }
+            else if (result != entry.expected) {
+                fmt::print("> {:60s} <-- \"{}\"  expected: \"{}\"\n", fmt::format("\"{}\"", result), entry.raw_name, entry.expected);
+                ++errors;
+            }
+            else if (verbose)
+                fmt::print("  {:60s} <-- \"{}\"\n", fmt::format("\"{}\"", result), entry.raw_name);
+        }
+        catch (std::exception& err) {
+            fmt::print("> {}: {}\n", entry.raw_name, err.what());
+            throw;
+        }
+    }
+    // fmt::print("{}\n", messages.report());
+    if (errors)
+        fmt::print("> {} errors found", errors);
+    return errors;
+
+} // passage_parsing_test
+
+// ----------------------------------------------------------------------
---
+++ cc/virus/passage-parse.cc
@@ -0,0 +1,11 @@
+#include "virus/passage-parse.hh"
+
+// ======================================================================
+
+std::string ae::virus::passage::parse(std::string_view source, parse_settings& settings, Messages& messages, const MessageLocation& location)
+{
+    return std::string{source};
+
+} // ae::virus::passage::parse
+
+// ----------------------------------------------------------------------
