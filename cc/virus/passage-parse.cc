a1582a7158a22a4878a364ab484cf83952276c15
Eugene Skepner <github@skepner.eu>
2021-11-13 11:55:05 +0100

    passage parser

--- cc/virus/passage-parse.cc
+++ cc/virus/passage-parse.cc
@@ -91,13 +91,15 @@ namespace ae::virus::passage
             static constexpr auto ori = just_or + I;
             static constexpr auto just_cs = C + S;
             static constexpr auto cli = C + L + I;
-            static constexpr auto cond = dsl::peek(just_or) | dsl::peek(just_cs) | dsl::peek(cli);
+            static constexpr auto dir = D + I + R;
+            static constexpr auto cond = dsl::peek(just_or) | dsl::peek(just_cs) | dsl::peek(cli) | dsl::peek(dir);
 
             static constexpr auto rule = //
                 (dsl::peek(ori) >> ori + dsl::any) //
                 | (dsl::peek(just_or) >> just_or) //
                 | (dsl::peek(cli) >> cli + dsl::any) //
                 | (dsl::peek(just_cs) >> just_cs) //
+                | (dsl::peek(dir) >> dir + dsl::any) //
                 ;
 
             static constexpr auto value = lexy::callback<std::string>([]() { return "OR"; });
