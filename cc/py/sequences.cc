edc8eae3444176fec1b3d29401952134fc1b0df3
Eugene Skepner <github@skepner.eu>
2021-11-16 15:40:29 +0100

    querying seqdb

--- cc/py/sequences.cc
+++ cc/py/sequences.cc
@@ -42,9 +42,11 @@ void ae::py::sequences(pybind11::module_& mdl)
         .def("human", &SeqdbSelected::filter_human)                //
         .def("filter_host", &SeqdbSelected::filter_host, "host"_a) //
         .def(
-            "filter_name", [](SeqdbSelected& selected, const std::vector<std::string>& names) -> SeqdbSelected& { return selected.filter_name(names); }, "names"_a) //
+            "filter_name", [](SeqdbSelected& selected, const std::vector<std::string>& names) -> SeqdbSelected& { return selected.filter_name(names); }, "names"_a,
+            py::doc("if name starts with ~ use regex matching (~ removed), if name contains _ use seq_id matching")) //
         .def(
-            "filter_name", [](SeqdbSelected& selected, const std::string& name) -> SeqdbSelected& { return selected.filter_name(std::vector<std::string>{name}); }, "names"_a) //
+            "filter_name", [](SeqdbSelected& selected, const std::string& name) -> SeqdbSelected& { return selected.filter_name(std::vector<std::string>{name}); }, "names"_a,
+            py::doc("if name starts with ~ use regex matching (~ removed), if name contains _ use seq_id matching")) //
         .def(
             "sort",
             [](SeqdbSelected& selected, std::string_view sorting_order) -> SeqdbSelected& {
