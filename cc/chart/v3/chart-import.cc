71d1f8488d04c25cdd2d8beab6808e604fa499f9
Eugene Skepner <github@skepner.eu>
2022-03-01 17:28:31 +0100

    chart import

--- cc/chart/v3/chart-import.cc
+++ cc/chart/v3/chart-import.cc
@@ -13,6 +13,17 @@ void ae::chart::v3::Chart::write(const std::filesystem::path& filename) const
 
 // ----------------------------------------------------------------------
 
+// class unhandled_key : public std::runtime_error
+// {
+//   public:
+//     unhandled_key(std::initializer_list<std::string_view> keys) : std::runtime_error{fmt::format("[chart] unhandled \"{}\"", fmt::join(keys, "\":\""))} {}
+// };
+
+inline void unhandled_key(std::initializer_list<std::string_view> keys)
+{
+    fmt::print(stderr, ">> [chart] unhandled \"{}\"\n", fmt::join(keys, "\":\""));
+}
+
 void ae::chart::v3::Chart::read(const std::filesystem::path& filename)
 {
     Timeit ti{"importing chart", std::chrono::milliseconds{5000}};
@@ -27,16 +38,31 @@ void ae::chart::v3::Chart::read(const std::filesystem::path& filename)
                     if (const std::string_view ver{field.value()}; ver != "acmacs-ace-v1")
                         throw Error{"unsupported version: \"{}\"", ver};
                 }
+                else if (key == "c") {
+                    for (auto field_c : field.value().get_object()) {
+                        if (const std::string_view key_c = field_c.unescaped_key(); key_c.size() == 1) {
+                            switch (key_c[0]) {
+                                case 'i':
+                                    break;
+                                default:
+                                    unhandled_key({"c", key_c});
+                                    break;
+                            }
+                        }
+                        else if (key_c[0] != '?' && key_c[0] != ' ' && key_c[0] != '_')
+                            unhandled_key({"c", key_c});
+                    }
+                }
                 else if (key[0] != '?' && key[0] != ' ' && key[0] != '_')
-                    fmt::print(">> [chart] unhandled \"{}\"\n", key);
+                    unhandled_key({key});
             }
         }
         catch (simdjson_error& err) {
-            // throw Error{"> {} parsing error: {} at {} \"{}\"\n", filename.native(), err.what(), parser.current_location_offset(), parser.current_location_snippet(50)};
+            throw Error{"> {} parsing error: {} at {} \"{}\"\n", filename.native(), err.what(), parser.current_location_offset(), parser.current_location_snippet(50)};
         }
     }
     catch (simdjson_error& err) {
-        throw Error{"> {} json parser creation error: {} (UNESCAPED_CHARS means a char < 0x20)\n", 5, 6}; // filename.native(), err.what()};
+        throw Error{"> {} json parser creation error: {} (UNESCAPED_CHARS means a char < 0x20)\n", filename.native(), err.what()};
     }
 
 } // ae::chart::v3::Chart::read
