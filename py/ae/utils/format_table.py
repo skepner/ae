369076a2aab512643f74f1d5651bea1f8f59bf23
Eugene Skepner <github@skepner.eu>
2022-03-23 17:02:21 +0100

    chart-titer-merge-report

--- py/ae/utils/format_table.py
+++ py/ae/utils/format_table.py
@@ -2,38 +2,67 @@ import math
 
 # ----------------------------------------------------------------------
 
-def format_table(table: list, field_sep: str =" ", format_field: callable = None):
-    """format_field is a function of two arguments (field, width: int) to format the field and properly align it, returns str
+class Formatter:
+
+    # do not call it format because str has format method
+    def fmt(self, field, width: int):
+        if hasattr(field, "fmt"):
+            return field.fmt(width)
+        elif isinstance(field, int):
+            return f"{field:{width}d}"
+        elif isinstance(field, float):
+            return f"{field:{width}.16f}"
+        else:
+            return f"{str(field):{width}s}"
+
+class ValueFormatter:
+
+    def __init__(self, value):
+        self.value = value
+
+    def width(self) -> int:
+        return len(str(self.value))
+
+class Centered (ValueFormatter):
+
+    def fmt(self, width: int) -> str:
+        return f"{str(self.value):^{width}s}"
+
+class RightAligned (ValueFormatter):
+
+    def fmt(self, width: int) -> str:
+        return f"{str(self.value):>{width}s}"
+
+# ----------------------------------------------------------------------
+
+def format_table(table: list, field_sep: str =" ", formatter: Formatter = None):
+    """formatter is an instance of class derived from Formatter, it may override fmt method.
     """
     if not table:
         return ""
     if isinstance(table[0], list):
-        return format_list_of_lists(table, field_sep=field_sep, format_field=format_field)
+        return format_list_of_lists(table, field_sep=field_sep, formatter=formatter)
     else:
-        return format_list_of_dicts(table, field_sep=field_sep, format_field=format_field)
+        return format_list_of_dicts(table, field_sep=field_sep, formatter=formatter)
 
 # ----------------------------------------------------------------------
 
-def format_list_of_lists(table: list, field_sep: str =" ", format_field: callable = None):
-    if format_field is None:
-        format_field = format_field_default
+def format_list_of_lists(table: list, field_sep: str =" ", formatter: Formatter = None):
+
+    def calculate_width(field):
+        if hasattr(field, "width"):
+            return field.width()
+        elif isinstance(field, float) and not math.isnan(field):
+            return len(str(int(col))) + 17
+        else:
+            return len(str(field))
+
+    if not formatter:
+        formatter = Formatter()
     widths = [0] * len(table[0])
     for row in table:
         for col_no, col in enumerate(row):
-            if isinstance(col, float) and not math.isnan(col):
-                widths[col_no] = max(widths[col_no], len(str(int(col))))
-            else:
-                widths[col_no] = max(widths[col_no], len(str(col)))
-    return "\n".join(field_sep.join(format_field(field, widths[field_no]) for field_no, field in enumerate(row)) for row in table)
-
-# ----------------------------------------------------------------------
-
-def format_field_default(field, width: int):
-    if isinstance(field, int):
-        return f"{field:{width}d}"
-    elif isinstance(field, float):
-        return f"{field:{width + 17}.16f}"
-    else:
-        return f"{str(field):{width}s}"
+            widths[col_no] = max(widths[col_no], calculate_width(col))
+    return "\n".join(field_sep.join(formatter.fmt(field, widths[field_no]) for field_no, field in enumerate(row)) for row in table)
 
 # ----------------------------------------------------------------------
