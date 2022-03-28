e67a57ee7164d4919651c251b321c682de0bd93c
Eugene Skepner <github@skepner.eu>
2022-03-28 15:39:14 +0200

    serum circles

--- cc/chart/v3/serum-circles.cc
+++ cc/chart/v3/serum-circles.cc
@@ -10,15 +10,28 @@ namespace ae::chart::v3
 
 ae::chart::v3::serum_circles_t ae::chart::v3::serum_circles(const Chart& chart, const Projection& projection, serum_circle_fold fold)
 {
-    // chart.set_homologous(acmacs::chart::find_homologous::all);
+    const auto column_bases = chart.column_bases(projection.minimum_column_basis());
+    serum_circles_t circles;
 
     for (const auto sr_no : chart.sera().size()) {
-        AD_DEBUG("homologous SR {:4d} \"{}\": {}", sr_no, chart.sera()[sr_no].designation(), chart.antigens().homologous(chart.sera()[sr_no]));
-        for (auto ag_no : chart.antigens().homologous(chart.sera()[sr_no])) {
+        // AD_DEBUG("homologous SR {:4d} \"{}\": {}", sr_no, chart.sera()[sr_no].designation(), chart.antigens().homologous(chart.sera()[sr_no]));
+        auto& serum_data = circles.sera.emplace_back(sr_no, column_bases[sr_no], fold);
+        for (const auto ag_no : chart.antigens().homologous(chart.sera()[sr_no])) {
+            if (const auto& titer = chart.titers().titer(ag_no, sr_no); titer.is_regular()) {
+                serum_data.antigens.push_back({.antigen_no = ag_no,
+                                               .titer = titer,
+                                               .theoretical = std::nullopt, // acmacs::chart::serum_circle_theoretical(ag_no, sr_no, chart, projection_no, 2.0, verbose),
+                                               .empirical = std::nullopt, // acmacs::chart::serum_circle_empirical(ag_no, sr_no, chart, projection_no, 2.0, verbose)});
+                                               .reason = serum_circle_failure_reason::good
+                    });
+            }
+            else {
+                serum_data.antigens.push_back({.antigen_no = ag_no, .titer = titer, .reason = serum_circle_failure_reason::non_regular_homologous_titer});
+            }
         }
     }
 
-    return serum_circles_t{};
+    return circles;
 
 } // ae::chart::v3::serum_circles
 
--- cc/chart/v3/serum-circles.hh
+++ cc/chart/v3/serum-circles.hh
@@ -1,3 +1,5 @@
+#include <optional>
+
 #include "utils/named-type.hh"
 #include "chart/v3/chart.hh"
 
@@ -6,12 +8,39 @@
 namespace ae::chart::v3
 {
     using serum_circle_fold = named_double_t<struct serum_circle_fold_tag>;
+    enum class serum_circle_failure_reason { not_calculated, good, non_regular_homologous_titer, titer_too_low, serum_disconnected, antigen_disconnected };
 
-    class serum_circles_t
+    struct serum_circle_antigen_t
     {
+        antigen_index antigen_no;
+        Titer titer;
+        std::optional<double> theoretical{};
+        std::optional<double> empirical{};
+        serum_circle_failure_reason reason{serum_circle_failure_reason::not_calculated};
+        double min_radius{2.0}; // Derek 2020-09-16 13:16 (Influenza B report and sig pages)
+
+        bool valid_theoretical() const { return theoretical.has_value(); }
+        bool valid_empirical() const { return empirical.has_value(); }
+    };
+
+    struct serum_circle_serum_t
+    {
+        serum_index serum_no;
+        double column_basis;
+        serum_circle_fold fold;
+        std::vector<serum_circle_antigen_t> antigens{};
+
+        serum_circle_serum_t(serum_index sr_no, double cb, serum_circle_fold a_fold) : serum_no{sr_no}, column_basis{cb}, fold{a_fold} {}
+        bool valid() const { return !antigens.empty(); }
+    };
+
+    struct serum_circles_t
+    {
+        std::vector<serum_circle_serum_t> sera{};
     };
 
     serum_circles_t serum_circles(const Chart& chart, const Projection& projection, serum_circle_fold fold);
-}
+
+} // namespace ae::chart::v3
 
 // ----------------------------------------------------------------------
