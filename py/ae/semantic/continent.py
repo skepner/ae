e6914fc46f74d3cb18a5c7470ea9c71d3fb228f0
Eugene Skepner <github@skepner.eu>
2022-05-25 17:34:58 +0200

    semantic attribute: country, continent

--- py/ae/semantic/continent.py
+++ py/ae/semantic/continent.py
@@ -5,9 +5,13 @@ import ae_backend
 
 def semantic(chart: ae_backend.chart_v3.Chart):
     """Set continent ("C9") and country ("c9") semantic attributes for all antigens and sera"""
-    locdb = ae_backend.locdb_v3.locdb()
-
-    loc = "CAMBRIDGE"
-    print(f"{loc} -- {locdb.country(loc)} -- {locdb.continent(locdb.country(loc))}", file=sys.stderr)
+    for selector in [chart.select_all_antigens, chart.select_all_sera]:
+        for ag_no, antigen in selector():
+            if parsed := ae_backend.virus.name_parse(antigen.name()):
+                if parsed.parts.continent:
+                    print(f">>>> {antigen.name()} -- {parsed.parts.continent} -- {parsed.parts.country}", file=sys.stderr)
+                    antigen.semantic.set("C9", parsed.parts.continent)
+                if parsed.parts.country:
+                    antigen.semantic.set("c9", parsed.parts.country)
 
 # ======================================================================
