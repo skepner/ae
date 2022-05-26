7722c9a47db2530925dd7d639f039042ef659f57
Eugene Skepner <github@skepner.eu>
2022-05-26 16:59:31 +0200

    adding semantic styles: vaccines

--- py/ae/semantic/vaccine.py
+++ py/ae/semantic/vaccine.py
@@ -121,49 +121,4 @@ def plot_style(chart: ae_backend.chart_v3.Chart, name: str = "-vaccines") -> set
         # print(f">>>> {no} {antigen.designation()}", file=sys.stderr)
     return set([name])
 
-# ----------------------------------------------------------------------
-
-# def vaccine(chart: ae_backend.chart_v3.Chart, name: str, year: str = None, passage: str = None, surrogate: bool = False, result: Result = None, **ignored) -> Result:
-#     """Add "V" semantic attribute to the vaccine antigens.
-#     passage: "egg", "cell", "reassortant"
-#     """
-#     # print(f"> {name} {passage} [{year}]")
-#     layout = chart.projection().layout() if chart.number_of_projections() else None
-#     vac_name = virus.add_subtype_prefix(chart, name)
-#     if result is None:
-#         result = Result(chart)
-#     subreport = {}
-#     for psg in (sPassages if passage is None else [passage]):
-#         antigens = chart.select_antigens(lambda ag: ag.name == vac_name and ag.passage_is(psg) and layout.connected(ag.point_no))
-#         if len(antigens):
-#             if result._has_layers and len(antigens) > 1:
-#                 antigens.sort_by_number_of_layers_descending()
-#             antigens[0][1].semantic.vaccine((year or "") + ("s" if surrogate else ""))
-#             subreport[psg] = antigens
-#     if subreport:
-#         result.data[name] = {"year": year, "surrogate": surrogate, **subreport}
-#     return result
-
-# # ----------------------------------------------------------------------
-
-# def vaccines(chart: ae_backend.chart_v3.Chart, entries: list) -> Result:
-#     result = Result(chart)
-#     for en in entries:
-#         vaccine(chart=chart, result=result, **en)
-#     return result
-
-# ----------------------------------------------------------------------
-
-# def vaccines_and_plot_style(chart: ae_backend.chart_v3.Chart, entries: list, modifier: dict = {"outline": "black", "rais": True, "size": 70, "only": "antigens"}, report: bool = True) -> str:
-#     if result := vaccines(chart=chart, entries=entries):
-#         style_name = "-vaccines"
-#         style = chart.styles()[style_name]
-#         style.priority = 1000
-#         style.add_modifier(selector=["V", ""], **modifier)
-#         if report:
-#             print(result.report(), file=sys.stderr)
-#         return style_name
-#     else:
-#         return None
-
 # ======================================================================
