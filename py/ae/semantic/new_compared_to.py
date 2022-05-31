3e324cae520e470bcb8355691f200cf2618c168b
Eugene Skepner <github@skepner.eu>
2022-05-31 12:58:01 +0200

    "-new-" styles

--- py/ae/semantic/new_compared_to.py
+++ py/ae/semantic/new_compared_to.py
@@ -11,3 +11,18 @@ def attributes(chart: ae_backend.chart_v3.Chart, previous_chart: ae_backend.char
         ag.semantic.set("new", new_attribute_value)
 
 # ======================================================================
+
+def style(chart: ae_backend.chart_v3.Chart, number_of_previous_charts: int, modifiers=list[dict], name_prefix: str = "-new", first_priority: int = 4500) -> set[str]:
+    """add "-new-1" and "-new-2" style marking antigens with "new" semantic attribute"""
+    snames = set()
+    priority = first_priority
+    for prev_no in range(number_of_previous_charts, 0, -1):
+        sname = f"{name_prefix}-{prev_no}"
+        style = chart.styles()[sname]
+        style.priority = priority
+        style.add_modifier(selector={"new": prev_no}, **modifiers[prev_no - 1], only="antigens")
+        snames.add(sname)
+        priority += 1
+    return snames
+
+# ======================================================================
