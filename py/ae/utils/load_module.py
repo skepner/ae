e5224ec8a58d0a110c57e9a50bb882b4609faa1a
Eugene Skepner <github@skepner.eu>
2022-06-07 12:51:45 +0200

    support for loading modules without .py suffix

--- py/ae/utils/load_module.py
+++ py/ae/utils/load_module.py
@@ -1,20 +1,23 @@
 from pathlib import Path
-import importlib.util
+import importlib.util, importlib.machinery
 
 # ----------------------------------------------------------------------
 
 sLoaded = {}
 
-def load(module_file: Path):
+def load(module_file: Path|str):
+    module_file = Path(module_file).resolve()
     try:
         return sLoaded[module_file]
     except KeyError:
         if module_file.exists():
-            spec = importlib.util.spec_from_file_location("ae", module_file)
-            mod = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(mod)
-            sLoaded[module_file] = mod
-            return mod
+            if spec := importlib.util.spec_from_file_location("ae", module_file, loader=importlib.machinery.SourceFileLoader("ae", str(module_file))):
+                mod = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(mod)
+                sLoaded[module_file] = mod
+                return mod
+            else:
+                raise RuntimeError(f"importlib.util.spec_from_file_location failed for \"{module_file}\"")
         else:
             print(f">> no {module_file}")
             sLoaded[module_file] = None
