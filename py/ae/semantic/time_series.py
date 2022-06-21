5661e61a4c6540422eeba82f7b5576c8d1b65a44
Eugene Skepner <github@skepner.eu>
2022-06-21 09:27:24 +0200

    time series end  is inclusive

--- py/ae/semantic/time_series.py
+++ py/ae/semantic/time_series.py
@@ -5,7 +5,7 @@ from . import front_style
 
 # ======================================================================
 
-def style(chart: ae_backend.chart_v3.Chart, name: str = "ts-", first: datetime.date|str = None, last: datetime.date|str = None, title_prefix: str = None, period: str = "month", priority: int = 5000, front_priority: int = 500):
+def style(chart: ae_backend.chart_v3.Chart, name: str = "ts-", first: datetime.date|str = None, last_inclusive: datetime.date|str = None, title_prefix: str = None, period: str = "month", priority: int = 5000, front_priority: int = 500):
     """period: "year", "month", "week" """
 
     if period == "year":
@@ -17,9 +17,9 @@ def style(chart: ae_backend.chart_v3.Chart, name: str = "ts-", first: datetime.d
     else:
         raise ValueError(f"time_series: uknown period: \"{period}\"")
 
-    first, last = get_antigen_date_range(chart=chart, first=first, last=last)
+    first, last_inclusive = get_antigen_date_range(chart=chart, first=first, last=last_inclusive)
     priority_inc = 0
-    for dfirst, dlast in time_series(first=first, last_inclusive=last, period=period):
+    for dfirst, dlast in time_series(first=first, last_inclusive=last_inclusive, period=period):
         sname = f"-{name}{dfirst.strftime(name_format)}"
         style = chart.styles()[sname]
         style.priority = priority + priority_inc
