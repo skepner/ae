73cd16168d51e191f1541dcee09c6f45c331c8ad
Eugene Skepner <github@skepner.eu>
2022-06-23 14:32:25 +0200

    title style in ts

--- py/ae/semantic/time_series.py
+++ py/ae/semantic/time_series.py
@@ -7,7 +7,7 @@ from . import front_style
 
 # ======================================================================
 
-def style(chart: ae_backend.chart_v3.Chart, time_series: TimeSeriesRange, name: str = "ts-", title_prefix: str = None, priority: int = 5000, front_priority: int = 500):
+def style(chart: ae_backend.chart_v3.Chart, time_series: TimeSeriesRange, name: str = "ts-", title_prefix: str = None, title_style: dict[str, object] = {}, priority: int = 5000, front_priority: int = 500):
     priority_inc = 0
     for dfirst, dlast in time_series.range_begin_end():
         sname = f"-{name}{dfirst.strftime(time_series.name_format_style())}"
@@ -16,7 +16,7 @@ def style(chart: ae_backend.chart_v3.Chart, time_series: TimeSeriesRange, name: 
         style.add_modifier(selector={"R": False, "!D": ["", dfirst.strftime("%Y-%m-%d")]}, hide=True, only="antigens")
         style.add_modifier(selector={"R": False, "!D": [dlast.strftime("%Y-%m-%d"), ""]}, hide=True, only="antigens")
         title = " ".join(en for en in [title_prefix, calendar.month_name[dfirst.month], str(dfirst.year)] if en)
-        front_style.add(chart=chart, style_name=sname[1:], references=["-reset", "-continent", "-new-1-big", sname, "-vaccines"], title=title, show_legend=True, legend_counter=True, style_priority=front_priority + priority_inc)
+        front_style.add(chart=chart, style_name=sname[1:], references=["-reset", "-continent", "-new-1-big", sname, "-vaccines"], title=title, title_style=title_style, show_legend=True, legend_counter=True, style_priority=front_priority + priority_inc)
         priority_inc += 1
 
 # ======================================================================
