2da2b8c617ddcc88479b21cfafd59442faf68c9c
Eugene Skepner <github@skepner.eu>
2022-06-24 13:23:02 +0200

    older_6_months_date

--- py/ae/semantic/older_than.py
+++ py/ae/semantic/older_than.py
@@ -6,7 +6,7 @@ import ae_backend
 def attributes(chart: ae_backend.chart_v3.Chart, conferencence_date: datetime.date):
     """add semantic attributes "o6m" and "o12m" to test antigens that are older than 6 months and 12 months at the first day of month of the conference"""
     ags = {"o6m": 0, "o12m": 0, "!o6m": 0, "!o12m": 0}
-    for attr, last_date in [["o6m", _o6m_date(conferencence_date).strftime("%Y-%m-%d")], ["o12m", _o12m_date(conferencence_date).strftime("%Y-%m-%d")]]:
+    for attr, last_date in [["o6m", older_6_months_date(conferencence_date).strftime("%Y-%m-%d")], ["o12m", older_12_months_date(conferencence_date).strftime("%Y-%m-%d")]]:
         ags[f"{attr}-last"] = last_date
         for no, ag in chart.select_all_antigens():
             if ag.date() < last_date:
@@ -16,14 +16,14 @@ def attributes(chart: ae_backend.chart_v3.Chart, conferencence_date: datetime.da
                 ags[f"!{attr}"] += 1
     print(f">>>> {ags}", file=sys.stderr)
 
-def _o6m_date(conferencence_date: datetime.date) -> datetime.date:
+def older_6_months_date(conferencence_date: datetime.date) -> datetime.date:
     date = conferencence_date.replace(day=1)
     if date.month > 6:
         return date.replace(month=date.month - 6)
     else:
         return date.replace(year=date.year - 1, month=date.month + 6)
 
-def _o12m_date(conferencence_date: datetime.date) -> datetime.date:
+def older_12_months_date(conferencence_date: datetime.date) -> datetime.date:
     return conferencence_date.replace(year=conferencence_date.year - 1, day=1)
 
 # ======================================================================
@@ -37,11 +37,11 @@ def style(chart: ae_backend.chart_v3.Chart):
         style.add_modifier(selector={attr: True}, outline="grey", fill="grey", lower=True, only="antigens")
 
 def since_6m_label(conferencence_date: datetime.date) -> str:
-    date = _o6m_date(conferencence_date=conferencence_date)
+    date = older_6_months_date(conferencence_date=conferencence_date)
     return f"{calendar.month_name[date.month]} {date.year}"
 
 def since_12m_label(conferencence_date: datetime.date) -> str:
-    date = _o12m_date(conferencence_date=conferencence_date)
+    date = older_12_months_date(conferencence_date=conferencence_date)
     return f"{calendar.month_name[date.month]} {date.year}"
 
 # ======================================================================
