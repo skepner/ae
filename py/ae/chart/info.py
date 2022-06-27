5e6ecabdc226cb38bbb76011756b3489ada3181f
Eugene Skepner <github@skepner.eu>
2022-06-27 17:57:05 +0200

    ae.chart.info

--- py/ae/chart/info.py
+++ py/ae/chart/info.py
@@ -4,20 +4,20 @@ import ae_backend
 
 # ======================================================================
 
-def info(chart: ae_backend.chart_v3.Chart, show_all: bool) -> str:
+def info(chart: ae_backend.chart_v3.Chart, show_projections: bool = False, show_forced_column_bases: bool = False, show_sources: bool = False) -> str:
 
     num_projections = chart.number_of_projections()
     nprj = f"projections:{num_projections}" if num_projections else "no projections"
 
     fcb = None
     if chart_forced_column_bases := chart.forced_column_bases():
-        if show_all:
+        if show_forced_column_bases:
             fcb = "  forced column bases: see after sources below"
         else:
             fcb = f"  forced column bases: {chart_forced_column_bases}"
 
     projections = ""
-    if show_all and num_projections:
+    if show_projections and num_projections:
         for projection_no in range(num_projections):
             projection = chart.projection(projection_no)
             projections += f"{projection_no:3d}  {projection.stress():11.6f} >={projection.minimum_column_basis()}"
@@ -36,23 +36,23 @@ def info(chart: ae_backend.chart_v3.Chart, show_all: bool) -> str:
                 projections += f" unmov-last:{unmovable_in_the_last_dimension}"
             projections += "\n"
 
+    info = chart.info()
     sources = None
+    if show_sources and (num_sources := info.number_of_sources()):
+        sources = f"sources {num_sources}\n"
+        for source_no in range(num_sources):
+            source = info.source(source_no)
+            sources += f"{source_no:3d} {source.date()} {source.name()}\n"
+        sources += "\n"
+
     colbases = None
-    if show_all:
-        info = chart.info()
-        if num_sources := info.number_of_sources():
-            sources = f"sources {num_sources}\n"
-            for source_no in range(num_sources):
-                source = info.source(source_no)
-                sources += f"{source_no:3d} {source.date()} {source.name()}\n"
-            sources += "\n"
-        if chart_forced_column_bases:
-            colbases = "forced column bases\n"
-            for no, sr in chart.select_all_sera():
-                fcb = f"{chart_forced_column_bases[no]:11.8f}"
-                fcb = re.sub(r"0+$", lambda m: " " * len(m.group(0)), fcb)
-                fcb = re.sub(r"\. ", "  ", fcb)
-                colbases += f"{no:3d}     {fcb}   {sr.name()} {sr.serum_id()} {sr.passage()}\n"
+    if show_forced_column_bases and chart_forced_column_bases:
+        colbases = "forced column bases\n"
+        for no, sr in chart.select_all_sera():
+            fcb = f"{chart_forced_column_bases[no]:11.8f}"
+            fcb = re.sub(r"0+$", lambda m: " " * len(m.group(0)), fcb)
+            fcb = re.sub(r"\. ", "  ", fcb)
+            colbases += f"{no:3d}     {fcb}   {sr.name()} {sr.serum_id()} {sr.passage()}\n"
 
     return "\n".join(en for en in [
         f"{chart.name(0)} {chart.number_of_antigens()}:{chart.number_of_sera()} {nprj}",
