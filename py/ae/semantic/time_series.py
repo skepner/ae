87d4776ef7fcf8e21aba06febc9e82061d7dfe3f
Eugene Skepner <github@skepner.eu>
2022-06-29 11:07:52 +0200

    support for different vaccine coloring in clades and ts styles

--- py/ae/semantic/time_series.py
+++ py/ae/semantic/time_series.py
@@ -8,7 +8,7 @@ from .style import style_with_one_modifier
 
 # ======================================================================
 
-def style(chart: ae_backend.chart_v3.Chart, time_series: TimeSeriesRange, name: str = "ts-", title_prefix: str = None, title_style: dict[str, object] = {}, priority: int = 5000, front_priority: int = 500):
+def style(chart: ae_backend.chart_v3.Chart, time_series: TimeSeriesRange, name: str = "ts-", vaccine_style_name: str = "-vaccines", title_prefix: str = None, title_style: dict[str, object] = {}, priority: int = 5000, front_priority: int = 500):
     priority_inc = 0
     for dfirst, dlast in time_series.range_begin_end():
         sname = f"-{name}{dfirst.strftime(time_series.name_format_style())}"
@@ -17,7 +17,7 @@ def style(chart: ae_backend.chart_v3.Chart, time_series: TimeSeriesRange, name: 
         style.add_modifier(selector={"R": False, "!D": ["", dfirst.strftime("%Y-%m-%d")]}, hide=True, only="antigens")
         style.add_modifier(selector={"R": False, "!D": [dlast.strftime("%Y-%m-%d"), ""]}, hide=True, only="antigens")
         title = " ".join(en for en in [title_prefix, calendar.month_name[dfirst.month], str(dfirst.year)] if en)
-        front_style.add(chart=chart, style_name=sname[1:], references=["-reset", "-continent", "-ts-old-new", sname, "-vaccines"], title=title, title_style=title_style, show_legend=True, legend_counter=True, style_priority=front_priority + priority_inc)
+        front_style.add(chart=chart, style_name=sname[1:], references=["-reset", "-continent", "-ts-old-new", sname, vaccine_style_name], title=title, title_style=title_style, show_legend=True, legend_counter=True, style_priority=front_priority + priority_inc)
         priority_inc += 1
 
 # ======================================================================
