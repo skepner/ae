c32f6a7ccfaf67fc7cc4e147ef44cbbe33da05c6
Eugene Skepner <github@skepner.eu>
2022-07-21 09:04:20 +0200

    bug fix

--- py/ae/sequences/source/fasta.py
+++ py/ae/sequences/source/fasta.py
@@ -2,7 +2,7 @@ import sys, re, io, pprint
 from pathlib import Path
 from typing import Union
 
-import ae_backend
+import ae_backend.raw_sequence, ae_backend.seqdb
 from .parse import Context, parse_name, parse_date, parse_passage
 
 # ======================================================================
@@ -73,10 +73,10 @@ def regular_name_parser(name: str, lab_hint: str, context: Context):
 # writer
 # ======================================================================
 
-def write(filename_or_stream: Union[io.TextIOWrapper, Path], selected :ae_backend.seqdb.Selected, aa: bool, wrap_pos: int = 0, truncate_at: int = None, name=lambda ref: ref.seq_id(), expand_too_short = False):
+def write(filename_or_stream: Union[io.TextIOWrapper, Path], selected: ae_backend.seqdb.Selected, aa: bool, wrap_pos: int = 0, truncate_at: int = 0, name=lambda ref: ref.seq_id(), expand_too_short = False):
     def do_wrap(data: str):
         if wrap_pos:
-            return "\n".join(data[i:i+wrap_pos] for i in range(0, len(data), wrap_pos))
+            return "\n".join(data[i:i + wrap_pos] for i in range(0, len(data), wrap_pos))
         else:
             return data
 
@@ -87,7 +87,7 @@ def write(filename_or_stream: Union[io.TextIOWrapper, Path], selected :ae_backen
             fil = filename_or_stream.open("w")
     else:
         fil = filename_or_stream
-    max_nuc, max_aa = selected.max_length();
+    max_nuc, max_aa = selected.max_length()
     for ref in selected:
         if aa:
             seq = str(ref.aa)
@@ -97,7 +97,7 @@ def write(filename_or_stream: Union[io.TextIOWrapper, Path], selected :ae_backen
             seq = str(ref.nuc)
             if expand_too_short:
                 seq += "-" * (max_nuc - len(seq))
-        if truncate_at is not None and len(seq) > truncate_at:
+        if truncate_at > 0 and len(seq) > truncate_at:
             seq = seq[:truncate_at]
         fil.write(f">{name(ref)}\n{do_wrap(str(seq))}\n")
     if isinstance(filename_or_stream, (Path, str)) and str(filename_or_stream) != "-":
