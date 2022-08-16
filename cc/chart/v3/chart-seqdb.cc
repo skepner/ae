9ce4191db35b31acadb806693fe39fb3e2be1f83
Eugene Skepner <github@skepner.eu>
2022-08-16 11:10:09 +0200

    verbose reporting of populating chart from seqdb

--- cc/chart/v3/chart-seqdb.cc
+++ cc/chart/v3/chart-seqdb.cc
@@ -11,7 +11,14 @@ std::pair<size_t, size_t> ae::chart::v3::populate_from_seqdb(Chart& chart, bool 
     const auto& seqdb = ae::sequences::seqdb_for_subtype(chart.info().virus_subtype());
     const ae::sequences::Clades clades{ae::sequences::Clades::load_from_default_file};
 
-    const auto update = [&clades](auto& ag_sr, auto& selected) {
+    const auto ag_or_sr = [](const auto& ag_sr) {
+        if (std::is_same_v<std::decay_t<decltype(ag_sr)>, Antigen>)
+            return "AG";
+        else
+            return "SR";
+    };
+
+    const auto update = [&clades, report_matched, ag_or_sr](auto& ag_sr, size_t no, auto& selected) {
         selected.find_masters();
         selected.find_clades(clades);
         const auto& ref = selected.at(0);
@@ -21,17 +28,18 @@ std::pair<size_t, size_t> ae::chart::v3::populate_from_seqdb(Chart& chart, bool 
         ag_sr.semantic().clades(ref.clades);
         if (const auto& insertions = ref.seq->aa_insertions; !insertions.empty())
             ag_sr.aa_insertions(insertions);
+        AD_INFO(report_matched, "{} {:4d} {:70s}  {}", ag_or_sr(ag_sr), no, ag_sr.designation(), ref.clades);
     };
 
-    const auto populate = [update, &seqdb, report_matched](auto& ag_srs) -> size_t {
+    const auto populate = [update, &seqdb](auto& ag_srs) -> size_t {
         size_t populated{0};
+        size_t no{0};
         for (auto& ag_sr : ag_srs) {
             auto selected_by_name = seqdb.select_by_name(ag_sr.name());
             selected_by_name->filter_name(ag_sr.name(), ag_sr.reassortant(), ag_sr.passage().to_string());
             if (!selected_by_name->empty()) {
-                update(ag_sr, *selected_by_name);
+                update(ag_sr, no, *selected_by_name);
                 ++populated;
-                AD_INFO(report_matched, "{}", ag_sr.designation());
             }
             else if constexpr (std::is_same_v<std::decay_t<decltype(ag_sr)>, chart::v3::Antigen>) {
                 if (!ag_sr.lab_ids().empty()) { // match by lab_id if match by name failed (e.g. CDC name in old tables)
@@ -39,13 +47,13 @@ std::pair<size_t, size_t> ae::chart::v3::populate_from_seqdb(Chart& chart, bool 
                         auto selected_by_seq_id = seqdb.select_all();
                         selected_by_seq_id->filter_lab_id(lab_id);
                         if (!selected_by_seq_id->empty()) {
-                            update(ag_sr, *selected_by_seq_id);
+                            update(ag_sr, no, *selected_by_seq_id);
                             ++populated;
-                            AD_INFO(report_matched, "{}", ag_sr.designation());
                         }
                     }
                 }
             }
+            ++no;
         }
         // AD_DEBUG("populated {}", populated);
         return populated;
